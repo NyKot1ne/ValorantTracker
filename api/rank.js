fd33efc9653fcb1b3246701a9832384368956c48
NyKot1ne <tiago.perrin@supinfo.com>
2025-10-07 11:34:56 +0200

    Update rank.js

--- api/rank.js
+++ api/rank.js
@@ -4,19 +4,28 @@ export default async function handler(req, res) {
   const region = "eu";
 
   try {
-    const response = await fetch(`https://api.henrikdev.xyz/valorant/v2/mmr/${region}/${name}/${tag}`);
+    const response = await fetch(
+      `https://api.henrikdev.xyz/valorant/v2/mmr/${region}/${name}/${tag}`
+    );
+
+    if (!response.ok) {
+      return res.status(404).send("Impossible de récupérer le rang.");
+    }
+
     const data = await response.json();
 
     if (!data.data?.current_data) {
-      return res.status(404).send("Impossible de récupérer le rang actuellement.");
+      return res.status(404).send("Aucune donnée disponible.");
     }
 
     const rank = data.data.current_data.currenttierpatched;
     const rr = data.data.current_data.ranking_in_tier;
     const peak = data.data.highest_rank.patched_tier;
 
-    res.status(200).send(`LA BETE DE FOIRE est actuellement ${rank} (${rr} RR) | Peak : ${peak}`);
-  } catch (error) {
-    res.status(500).send("Erreur lors de la récupération du rang.");
+    return res
+      .status(200)
+      .send(`LA BETE DE FOIRE est ${rank} (${rr} RR) | Peak : ${peak}`);
+  } catch (err) {
+    return res.status(500).send("Erreur lors de la récupération du rang.");
   }
 }
