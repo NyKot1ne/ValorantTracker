23c5fa214a62e845908ed8eea9a1f5490aeeab18
NyKot1ne <tiago.perrin@supinfo.com>
2026-06-14 10:52:24 +0200

    Fix formatting of RR replacements in index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ export default {
       let result = text
         .replace("(🛡️ 0)", "")
         .replace(/\((\d+)\)/, "(+$1)")
-        .replace(", RR:", " |");
+        .replace(", RR: ", " | ")
+        .replace(/(\| \d+)/, "$1 RR");
 
       return new Response(result.trim());
     } catch (err) {
