5e3ff28243a72ac900e2c4a134c40d5611952220
NyKot1ne <tiago.perrin@supinfo.com>
2026-06-14 11:06:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 export default {
   async fetch(request) {
     try {
+      const url = new URL(request.url);
+
+      const parts = url.pathname.split("/").filter(Boolean);
+
+      if (parts.length < 2) {
+        return new Response(
+          "Utilisation : /pseudo/tag",
+          { status: 400 }
+        );
+      }
+
+      const pseudo = parts[0];
+      const tag = parts[1];
+
       const response = await fetch(
-        "https://vaccie.pythonanywhere.com/mmr/VIT%20nykotain/BARK/eu"
+        `https://vaccie.pythonanywhere.com/mmr/${encodeURIComponent(pseudo)}/${encodeURIComponent(tag)}/eu`
       );
 
       const text = await response.text();
@@ -14,8 +28,9 @@ export default {
         .replace(/(\| \d+)/, "$1 RR");
 
       return new Response(result.trim());
+
     } catch (err) {
       return new Response("Erreur Valorant");
     }
-  },
+  }
 };
